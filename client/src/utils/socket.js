1e4419a3d1b43f82f669b969606ecb2ab8fda11e
sarthakgarg07 <garsar07@gmail.com>
2025-06-21 12:59:37 +0530

    fix(socket): use vite api url for socket connection

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:5001';
+const SOCKET_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
 
 const socket = io(SOCKET_URL, {
   autoConnect: false, // We'll connect manually
